58529b1fcd474ad20333894b6984de01634b9f5f
Ian <824999404@qq.com>
2020-07-17 16:05:49 +0800

    Init

---
+++ kernel/welcome.c
@@ -0,0 +1,60 @@
+#include "type.h"
+#include "stdio.h"
+#include "const.h"
+#include "protect.h"
+#include "string.h"
+#include "fs.h"
+#include "proc.h"
+#include "tty.h"
+#include "console.h"
+#include "global.h"
+#include "proto.h"
+
+
+void welcomeAnimation(){
+	int t=200;
+	milli_delay(t);
+	int i = 0;
+	for (i = 1; i <= 100; ++i)
+	{
+		printf("-");
+	}
+	for (i = 1; i <= 100; ++i)
+	{
+		printf("\b");
+	}
+	for (i = 1; i <= 160; ++i)
+	{
+		printf(">");
+		milli_delay(t);
+	}
+
+	printf("\n");
+	milli_delay(t);
+	printf("\n");
+}
+
+PUBLIC void welcome(){   
+	printf("             ####    #######          ###    ######     #######              \n"); 
+ 	printf("             ####   #####                   ###  ####  ###  ####             \n");
+ 	printf("             ####   #####                  ###    #######    ###             \n");
+ 	printf("             ##### ######### #### ### ### ###      ### ###                   \n");
+ 	printf("             ##### ######### ###   ###### ###      ###  ######               \n");
+ 	printf("             ######## ###### ###   ###### ###      ###     #####             \n");
+ 	printf("             ######## ###### ###   ###### ###      ###       ###             \n");
+ 	printf("             ### #### ###### ###   ###### ###      ###        ###            \n");
+ 	printf("             ### ###  ###### ###   ######  ###    #######    ###             \n");
+ 	printf("             ###      ###### ###   ######   ###  ####  ###   ###             \n");
+ 	printf("             ###      ###### ###   ######    ######     #######              \n");                                       
+	printf(" |=================================MiniOS====================================|\n");
+	printf(" |                             1851197 Kaibin Zhou                           |\n");
+	printf(" |                             1851352 Rui Fu                                |\n");
+	printf(" |                             1851199 Zeyang Zhuang                         |\n");
+	printf(" |                             1851494 Yuqi Wang                             |\n");
+	printf(" |                             1851198 Jiayao Lv                             |\n");
+	printf(" |===========================================================================|\n");
+	printf("\n");
+	printf("\n");
+	//printf("help\n");
+}
+
